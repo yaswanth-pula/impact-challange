223e1501e38ab5112924cc96c9130ca86f6377cc
yaswanth-pula <35864576+Yaswanth899@users.noreply.github.com>
2021-01-05 19:20:30 +0530

    Navigation bar completed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CandidateList from "./components/CandidateList";
+import NavigationBar from "./components/NavigationBar";
 import "./app.css";
 
 function App() {
   return (
     <div className="app">
+      <NavigationBar />
       <CandidateList />
     </div>
   );
--- src/components/CandidateList.js
+++ src/components/CandidateList.js
@@ -3,19 +3,23 @@ import Candidate from "./Candidate";
 import "./candidate.css";
 
 function CandidateList() {
-  const [candidateList] = useApiFetch();
+  const [candidateList, isLoading] = useApiFetch();
 
   return (
     <div className="candidateList">
-      {candidateList.map((candidate) => {
-        return (
-          <Candidate
-            key={candidate.id}
-            imgSrc={candidate.Image}
-            name={candidate.name}
-          />
-        );
-      })}
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        candidateList.map((candidate) => {
+          return (
+            <Candidate
+              key={candidate.id}
+              imgSrc={candidate.Image}
+              name={candidate.name}
+            />
+          );
+        })
+      )}
     </div>
   );
 }
